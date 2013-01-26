ae166ebadc5a49249e762a4f8b892c1a39572b1a
Yoshiyuki MIYAGI <yosshi1202@gmail.com>
2013-01-26 22:18:07 +0900

    設定インストール用の処理を追加
     * gitignore
     * vim

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 
 DIR=`pwd`
+
+echo 'git setting'
 ln -s ${DIR}/git/dot.gitconfig ~/.gitconfig
+ln -s ${DIR}/git/dot.gitignore ~/.gitignore
+
+echo 'zsh setting'
 ln -s ${DIR}/zsh/dot.zshrc ~/.zshrc
+
+echo 'vim setting'
+ln -s ${DIR}/vim ~/.vim
+ln -s ~/.vim/dot.vimrc ~/.vimrc
 
