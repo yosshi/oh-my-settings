5e06f1adb459570e3a6eb62d3efcaf37c899ae41
Yoshiyuki MIYAGI <yosshi1202@gmail.com>
2013-01-29 14:18:39 +0900

    add brew install コマンド

---
+++ mac/README.md
@@ -0,0 +1,10 @@
+# defaults_hack.sh
+
+defaults コマンドで mac の各種設定を実行するスクリプト
+
+
+# brew_install.sh
+
+mac 用にパッケージをインストールするためのスクリプト
+* まず homebrew を入れておく
+
---
+++ mac/brew_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+brew install zsh
+brew install tree
+brew install git
+brew install tmux
+brew install wget
+brew install markdown
+
